8b2ff36af5b2fcaeeb19e0f8abb6a7a6d4f0ab7a
misterdai520 <misterdai520@163.com>
2016-03-08 18:34:11 +0800

    第二次改变

--- app/src/main/java/com/guigu/mygittest/MainActivity.java
+++ app/src/main/java/com/guigu/mygittest/MainActivity.java
@@ -2,12 +2,22 @@ package com.guigu.mygittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private ImageView girl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        girl = (ImageView) findViewById(R.id.girl);
+
+    }
+
+    public void change(View view){
+        girl.setBackgroundResource(R.drawable.adeaa01091f3d18e1571c903843a5b14);
     }
 }
